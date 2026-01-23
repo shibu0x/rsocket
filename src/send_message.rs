03b81e6cfe40323f5e295e46ac927044af513307
Shivendra Mishra <shivendramishra.sm93@gmail.com>
2026-01-24 02:37:21 +0530

    feat(response) : stream the data and frames to the client with all the flags and payload

--- src/send_message.rs
+++ src/send_message.rs
@@ -1,23 +1,25 @@
 use std::io::{Result, Write};
 use std::net::TcpStream;
 
-pub fn send_message(stream: &mut TcpStream, msg: &str) -> Result<()> {
-    let payload = msg.as_bytes();
-
+pub fn send_message(stream: &mut TcpStream, byte1:u8,payload:&[u8]) -> Result<()> {
     let mut frame = Vec::new();
 
-    //first byte
-    let fin = 1 << 7;
-    let opcode = 0x1;
-    frame.push(fin | opcode);
+    frame.push(byte1);
 
-    //second byte
-    let mask = 0x0;
-    let payload_length = payload.len() as u8;
-    frame.push(mask | payload_length);
+    let payload_len = payload.len();
+
+    if payload_len <126 {
+        frame.push(payload_len as u8);
+    } else if payload_len <= 65535 {
+        frame.push(126);
+        frame.extend_from_slice(&(payload_len as u16).to_be_bytes());
+    } else {
+        frame.push(127);
+        frame.extend_from_slice(&(payload_len as u64).to_be_bytes());
+    }
 
     frame.extend_from_slice(payload);
 
-    let _ = stream.write_all(&frame);
-    Ok(())
+    stream.write_all(&frame)
+
 }
